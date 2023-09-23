1ef570c7aea087a70c1a32b86e16354f9243649f
Riley <rileyg0035@gmail.com>
2023-09-23 17:32:28 -0300

    troubleshooting node

--- Develop/index.js
+++ Develop/index.js
@@ -4,59 +4,114 @@ const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 
+function promptForRepoInfo() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'repositoryName',
+        message: 'Enter the name of your repository:',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your repository:',
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Enter the author or owner of the repository:',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Enter the license for your repository:',
+      },
+    ])
+    .then((answers) => {
+      const readmeContent = generateREADME(answers);
+      writeREADME('README.md', readmeContent);
+      console.log('README.md file generated successfully.');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
 
-const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'Enter project title:',
-
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Enter project description',
-
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'Provide installation instructions:',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'Explain how to use your project:',
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'Choose a license for your project:',
-    choices: ['MIT', 'Apache 2.0', 'GPL', 'None'],
-  },
-  {
-    type: 'input',
-    name: 'contributing',
-    message: 'Explain how others can contribute to your project:',
-  },
-  {
-    type: 'input',
-    name: 'tests',
-    message: 'Provide testing instructions:',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'Enter your email address:',
-  },
-];
-  
-
-
-// TODO: Create a function to write README file
+// Call the function to start prompting for information
+promptForRepoInfo();
 
 
 // TODO: Create a function to initialize app
 
-// Function call to initialize app
+const fs = require('fs');
+
+// Function to generate the README content
+function generateREADME(answers) {
+  // Create the README content as a string
+  const readmeContent = `
+# ${answers.title}
+
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+`;
+}
+
+
+function writeREADME(filename, content) {
+  fs.writeFile(filename, content, (err) => {
+    if (err) {
+      console.error('Error writing README file:', err);
+    }
+  });
+}
+
+// Function to write README file
+// function writeREADME(filename, content) {
+//   fs.writeFile(filename, content, (err) => {
+//     if (err) {
+//       console.error('Error writing README file:', err);
+//     } else {
+//       console.log('README file generated successfully.');
+//     }
+//   });
+// }
+
+// // Function to initialize the application
+// function initializeApp() {
+//   // Define your array of questions here (as shown in previous examples)
+
+//   // Prompt the user for input
+//   inquirer
+//     .prompt(questions)
+//     .then((answers) => {
+//       const readmeContent = generateREADME(answers);
+
+//       // Write the README.md file
+//       writeREADME('README.md', readmeContent);
+//     });
+// }
+
+// // Call the initializeApp function to start the application
+// initializeApp();
+
+
+
+// // Function call to initialize app
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,75 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+
+// `;
+// }
+
+// module.exports = generateMarkdown;
+
+function renderLicenseBadge(license) {
+  if (license) {
+    return `[![License](https://img.shields.io/badge/License-${license}-brightgreen.svg)](https://opensource.org/licenses/${license})`;
+  }
+  return '';
+}
+
+function renderLicenseLink(license) {
+  if (license) {
+    return `[License](https://opensource.org/licenses/${license})`;
+  }
+  return '';
+}
+
+
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+  }
+  return '';
+}
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+${renderLicenseSection(data.license)}
 `;
+
 }
 
 module.exports = generateMarkdown;
