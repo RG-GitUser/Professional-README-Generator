132d31c44f071557a5e2466b7a48a99c51cf2951
Riley <rileyg0035@gmail.com>
2023-09-23 16:47:19 -0300

    added npm

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+
+
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Enter your project name: ', (projectName) => {
+  const template = `# ${projectName}\n\n## Description\n\n[Description of your project]\n\n## Installation\n\n[Installation instructions]\n\n## Usage\n\n[Usage instructions]\n`;
+
+  fs.writeFileSync('README.md', template, 'utf8');
+  console.log('README.md file created successfully.');
+  rl.close();
+});
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
-init();
+
